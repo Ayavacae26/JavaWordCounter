04f1a2d77b107e72d64b966ba7aba9acf29eb0ad
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-09-25 22:46:27 -0500

    Adding hashmap to my wordcount class

--- WordCount.java
+++ WordCount.java
@@ -12,16 +12,16 @@ public class WordCount {
 	public static void main(String[] args) {
 
 		// Here we are setting up the scanner to take in a file
-		Scanner textfile = new Scanner(System.in);
+		Scanner textFile = new Scanner(System.in);
 
-	    //File filename = new File("/Users/erikayavaca/Desktop/Erik Ayavaca.txt");
+	    //File filename = new File("/Users/erikayavaca/Desktop/Erik Ayavaca.txt"); Orginal input idea
 	
 	try {
 	    //Using the args(0) to put into the file into it
         File inputFile = new File(args[0]);
 
         //pointing to the file into the scanner
-        textfile = new Scanner(inputFile);
+        textFile = new Scanner(inputFile);
 
         // Not nesscary for this a that momment
         //while(scan.hasNextLine()) {
@@ -32,9 +32,18 @@ public class WordCount {
 		System.out.println("File is not found");
         // e.printStackTrace();
 	}
+		// going to use hashmap to keep track of the words
+		HashMap<String, Integer> wordCounter= new HashMap<String, Integer>();
 
+		while(textFile.hasNext() !=false){
+			/*
+			Storing the next scanned text into a new variable
+			 */
+			String text = textFile.next();
 
-
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
+			//Punctation clean uo. transform into lowercase and elminate punctuations
+			text = text.toLowerCase();
+			
+		}
 	}
 }
