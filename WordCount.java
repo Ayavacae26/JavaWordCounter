5820718e06ee44e8d0dd625966d129464122b1a9
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-09-25 22:20:55 -0500

    file scanner update.

--- WordCount.java
+++ WordCount.java
@@ -3,25 +3,34 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * By: Erik Ayavaca-Tirado
+ * Iteration 1
+ */
 
 public class WordCount {
 	public static void main(String[] args) {
 
 		// Here we are setting up the scanner to take in a file
-		Scanner scan = new Scanner(System.in);
+		Scanner textfile = new Scanner(System.in);
 
-	File filename = new File("/Users/erikayavaca/Desktop/Erik Ayavaca.txt");
+	    //File filename = new File("/Users/erikayavaca/Desktop/Erik Ayavaca.txt");
 	
 	try {
-		scan = new Scanner(filename);
-		while(scan.hasNextLine()) {
-	
-			System.out.println(scan.nextLine());
-		}
+	    //Using the args(0) to put into the file into it
+        File inputFile = new File(args[0]);
+
+        //pointing to the file into the scanner
+        textfile = new Scanner(inputFile);
+
+        // Not nesscary for this a that momment
+        //while(scan.hasNextLine()) {
+		    //System.out.println(scan.nextLine());
+		//}
 	}
 	catch(FileNotFoundException e) {
-		e.printStackTrace();
-		
+		System.out.println("File is not found");
+        // e.printStackTrace();
 	}
 
 
