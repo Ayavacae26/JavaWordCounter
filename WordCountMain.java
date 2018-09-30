b8e7bb84f260afb7dcdf2db3e03fa9b39c7ab0fc
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-09-30 12:14:53 -0500

    Created a main to call all funtions

---
+++ WordCountMain.java
@@ -0,0 +1,16 @@
+import java.io.* ;
+import java.util.Scanner;
+
+public class WordCountMain {
+    public static void main(String[] args){
+
+        File input = new File(args[0]);
+        WordCount files = new WordCount(input);
+        files.wordCount();
+
+
+
+
+
+    }
+}
