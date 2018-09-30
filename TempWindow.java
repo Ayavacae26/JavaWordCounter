0e01102554806f3479871fdde23136775014e035
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-09-30 16:16:02 -0500

    possibly finished the gui

--- TempWindow.java
+++ TempWindow.java
@@ -1,44 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties. * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javax.swing.*;
-import​ java.awt.event.ActionEvent;
-import​ java.awt.event.ActionListener;
-import​ java.io.File;
-import​ javax.swing.JButton;
-import​ javax.swing.JFileChooser;
-import​ javax.swing.JFrame;
-import​ javax.swing.JLabel;
-import​ ​javax.swing.JTextField​;
-import​ javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import ​javax.swing.JTextField​;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class TempWindow extends JFrame {
     JLabel label = new JLabel("Word Counter");
     JButton fileButton = new JButton("Choose Text File");
 
-    public TempWindow(){
+    public TempWindow() {
         this.setTitle("Word Counter File");
-        this.setBounds( 300, 400, 350, 250); ​
-        this.getContentPane().setLayout(​null); ​
-        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE​);
-​        this.label.setBounds(130, 60, 100, 30); ​
+        this.setBounds(300, 400, 350, 250); ​
+        this.getContentPane().setLayout(null); ​
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+​this.label.setBounds(130, 60, 100, 30); ​
         this.getContentPane().add(​label);
-​        this.​fileButton.setBounds(100, 100, 150, 60); ​
+​this.​fileButton.setBounds(100, 100, 150, 60); ​
         this.getContentPane().add(​fileButton); ​
-        this.​fileButton.addActionListener(new​ FileButtonListener());
+        this.​fileButton.addActionListener(new FileButtonListner());
     }
-            
 
+    private class FileButtonListner implements ActionListener {
 
-
-
-
-
-
+        @Override
+        public void actionPerfomed(ActionEvent e) {
+            System.out.println("hit the file button");
+            JFileChooser chooser = new JFileChooser();
+            chooser.setFileFilter(new FileNameExtensionFilter("TEXT FILES", "txt", "test"));
+            System.out.println("I created the file chooser");
+            int chooserSuccess = chooser.showOpenDialog(null);
+            System.out.println("I opened the file chooser, it returned" + chooserSuccess);
+            if (chooserSuccess == JFileChooser.APPROVE_OPTION) {
+                File chosenFile = chooser.getSelectedFile();
+                //pass this file to your function
+                System.out.println("You choose the file:" + chosenFile.getAbsolutePath());
+                System.out.println("You choose the file:" + chosenFile.getName());
+            } else {
+                System.out.println("You hit cancel");
+            }
+        }
 
     }
+}
 
 
 
