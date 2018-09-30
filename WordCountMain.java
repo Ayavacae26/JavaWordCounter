c5056329627128ab54c356178892893643add93a
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-09-30 16:55:49 -0500

    Gui pops up and file can be chosen but does not go into the word count function

--- WordCountMain.java
+++ WordCountMain.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class WordCountMain {
     public static void main(String[] args){
+        TempWindow tw = new TempWindow();
+        tw.setVisible(true);
 
         File input = new File(args[0]);
         WordCount files = new WordCount(input);
